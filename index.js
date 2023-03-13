4d83df3fb69db18cf39b814f37dae8439921b85a
nyamvularinga <nyamvularinga@gmail.com>
2023-03-13 22:50:15 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = (num1*num2)
+let num3 = 39
+let num4 = 5
+let mod = (num3%= num4)
+const random = 5
+let max = 20
+max.math (0,20)
